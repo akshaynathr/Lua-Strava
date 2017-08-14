9e827fa12abf47fa6f76abbadc96471217c0ba85
Akshaynath R <akshaynathr@gmail.com>
2017-08-15 00:53:38 +0530

    added login function, token authentication

--- examples/app.lua
+++ examples/app.lua
@@ -1,11 +1,12 @@
 local lapis = require("lapis")
 local config=require("lapis.config").get()
-local client=require("luastrava.client").Client
+local strava=require("luastrava.client").Client
 local app = lapis.Application()
 app:enable("etlua")
 local client_id='18243'
 local client_secret='99675b7ee65da2654be5c831cb09342079f5fdd9'
 
+local client=strava:new()
 
 app:match("index","/", function()
     --return "Hello"
@@ -13,8 +14,8 @@ app:match("index","/", function()
 end)
 
 
-app:get("login",function()
-     local url=client:authorization_url{client_id=client_id,redirect_uri='http://  localhost:8080/auth',scope='write'}
+app:get("/login",function()
+     local url=client:authorization_url{client_id=client_id,redirect_uri='http://localhost:8080/auth',scope='write'}
       print(url)
    return {redirect_to=url}
 end)
