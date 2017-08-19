4a3bc46cf81be7909ba2bd73bb05ff55b822c60d
Akshaynath R <akshaynathr@gmail.com>
2017-08-19 10:57:58 +0530

    added new page

--- examples/app.lua
+++ examples/app.lua
@@ -8,6 +8,46 @@ local client_secret='99675b7ee65da2654be5c831cb09342079f5fdd9'
 
 local client=strava:new()
 
+function table.val_to_str ( v )
+      if "string" == type( v ) then
+      	v = string.gsub( v, "\n", "\\n" )
+        if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+        	return "'" .. v .. "'"
+        end
+        return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+      else
+        return "table" == type( v ) and table.tostring( v ) or
+                                                    tostring( v )
+      end
+                                                  end
+
+
+      function table.key_to_str ( k )
+            if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+                    return k
+                      else
+                              return "[" .. table.val_to_str( k ) .. "]"
+                                end
+                            end
+
+                            function table.tostring( tbl )
+                                 local result, done = {}, {}
+                                   for k, v in ipairs( tbl ) do
+                                           table.insert( result, table.val_to_str( v ) )
+                                               done[ k ] = true
+                                                 end
+                                                   for k, v in pairs( tbl ) do
+                                                           if not done[ k ] then
+                                                                table.insert( result,
+                                                                  table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+                                                                      end
+                                                                        end
+                                                                          return "{" .. table.concat( result, "," ) .. "}"
+                                                                      end
+
+
+
+
 app:match("index","/", function()
     --return "Hello"
   return {render=true}
@@ -35,5 +75,12 @@ else return {redirect_to='/error'} end
 
                          
  end)
+
+ app:get("/main",function(self) 
+--    local year=2015
+    client:set_access_token(self.session.token)
+ local races=   client:get_races({year=2015})
+    return  table.tostring(races)
+    end)
 
 return app
